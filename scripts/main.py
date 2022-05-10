57d8134cf319a2b1051c9bd195de55fbcd72c0fe
rustamPy <kerimov.rustam@live.ru>
2022-05-10 10:17:57 +0200

    cc

--- scripts/main.py
+++ scripts/main.py
@@ -1,2 +1,16 @@
-def real(a):
-    return a
+import pandas as pd
+import os
+class Solution:
+    def __init__(self):
+        self.file=0
+    def create_sheet(self, name):
+
+        sh=pd.ExcelWriter('../data/data_sheet_'+name+'.xlsx', engine='xlsxwriter')
+        self.file = sh;
+        self.file.save()
+
+
+p=Solution()
+p.create_sheet('d')
+p.add_df({'Name': ['A', 'B', 'C', 'D'],'Age': [10, 0, 30, 50]})
+
